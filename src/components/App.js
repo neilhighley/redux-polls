22a6ec7ae5570d2ebe994ac77d2d0a511a0054da
Neil Highley <neil@neilhighley.com>
2018-04-03 03:17:00 +0000

    add loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
-
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
   componentDidMount(){
@@ -10,10 +11,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code.
+      {this.props.loading
+      ? <LoadingBar/>
+      : <Dashboard/>
+        
+      }
       </div>
     )
   }
 }
+function mapStateToProps({authedUser}){
+  return {
+    loading:authedUser===null
+  }
+}
 
-export default connect()(App)
+export default connect(mapStateToProps)(App)
